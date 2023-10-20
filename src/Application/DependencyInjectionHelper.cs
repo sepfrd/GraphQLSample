dd8581db39cbaf5e526c03f2922f2e0835a1a90f
Sepehr Frd <sepfrd@outlook.com>
2023-10-20 18:47:54 +0330

    Add DependencyInjectionHelper to Application layer.

---
+++ src/Application/DependencyInjectionHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjectionHelper
+{
+    public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+    {
+        services
+            .AddMediatR(configuration => configuration
+                .RegisterServicesFromAssembly(typeof(DependencyInjectionHelper).Assembly));
+
+        return services;
+    }
+}
