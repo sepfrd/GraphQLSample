073551e536c1a834bce74161beb03495ea0cd533
Sepehr Frd <sepfrd@outlook.com>
2023-11-27 01:05:17 +0330

    [bugfix] Remove sub total price from CreateOrderItemDto

--- src/Application/EntityManagement/OrderItems/Dtos/CreateOrderItemDto.cs
+++ src/Application/EntityManagement/OrderItems/Dtos/CreateOrderItemDto.cs
@@ -3,5 +3,4 @@ namespace Application.EntityManagement.OrderItems.Dtos;
 public record CreateOrderItemDto(
     int ProductExternalId,
     decimal UnitPrice,
-    decimal SubTotalPrice,
     int Quantity);
